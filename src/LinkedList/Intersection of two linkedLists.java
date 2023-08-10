c705eb81d8850190c9f4bd51149365fcb3cfd3b2
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-08-09 17:18:15 -0700

    Create Intersection of two linkedLists.java
    Use two pointers to find the intersection of two linked list.

---
+++ src/LinkedList/Intersection of two linkedLists.java
@@ -0,0 +1,44 @@
+/*
+*/
+Leetcode 160. Intersection of linkedList:
+https://leetcode.com/problems/intersection-of-two-linked-lists/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // Method 1: use a HashSet as a basic solution.
+        // but the time complexity is too slow!
+        /*
+        Set<ListNode> set = new HashSet<>();
+        while (headA != null) {
+            set.add(headA);
+            headA = headA.next;
+        }
+        while (headB != null) {
+            if (set.contains(headB)) {
+                return headB;
+            }
+            headB = headB.next;
+        }
+        return null;
+        */
+
+        // Method2: use two pointers
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+}
