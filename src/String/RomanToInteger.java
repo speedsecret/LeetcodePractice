fbf85b857ab5309c98d7b53b9566da5630d3f500
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-08-25 16:51:06 -0700

    Update RomanToInteger.java
    Offering one more solution to romanNumber to Integer.

--- src/String/RomanToInteger.java
+++ src/String/RomanToInteger.java
@@ -31,6 +31,29 @@ import java.util.Map;
 
 public class RomanToInteger {
 
+    // [Recommended]Method 1: using a HashMap<Character,Integer> to process char-based string.
+    // Don't skip any index, but rapidly adding or subtract elements.
+    public int romanToInt(String s) {         
+        private final Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                res -= map.get(s.charAt(i));
+            } else {
+                res += map.get(s.charAt(i));
+            }
+        }
+        return res;
+    }
+
+    // Method 2:
     // As Roman alphabet is limited number
     // we could use a hashMap to store string to mapping it to integer and then to seek adding it up
 
