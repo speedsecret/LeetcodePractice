7e2db34129b7c323b201a7a7480cd6bbab4aa6d9
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-08-29 12:34:43 -0700

    Create Subarray Sum Equals K.java
    Adding new code for subArray sum.

---
+++ src/PrefixSum/Subarray Sum Equals K.java
@@ -0,0 +1,49 @@
+/*
+Leetcode 560. Subarray Sum Equals K
+https://leetcode.com/problems/subarray-sum-equals-k/
+
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-1000 <= nums[i] <= 1000
+-107 <= k <= 107
+*/
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        // if sum[i] - sum[j] == 0
+        // which represent the elements in between i and j are zero
+        // if sum[i] - sum[j] == k
+        // wihch represent the elemtns in between i and j are k
+        // use this feature to build up an hashMap
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int prefixSum = 0;
+        int counter = 0;
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum += nums[i];
+            if (map.containsKey(prefixSum - k)) {
+                counter += map.get(prefixSum - k);
+            }
+
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+        }
+        return counter;
+    }
+}
+
+
