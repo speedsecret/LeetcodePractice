a1d64a869595d64ca6d72a414b2f97e633b2f6c6
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-09-01 17:13:04 -0700

    Create 64.Minimum Path Sum.java
    DP with no extra space.

---
+++ src/DP/64.Minimum Path Sum.java
@@ -0,0 +1,65 @@
+/*
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right,
+which minimizes the sum of all numbers along its path.
+Note: You can only move either down or right at any point in time.
+
+Example 1:
+Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+Output: 7
+Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+
+Example 2:
+Input: grid = [[1,2,3],[4,5,6]]
+Output: 12
+*/
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        // Method 1: use DP with an extra size n * n matrix then traverse from bottom right to top left iteratively
+        int m = grid.length, n = grid[0].length;
+        // actually there are 4 different position that applys to different DP strategies.
+        // starting point
+        // bottom level not the starting point
+        // rightMost col not the starting point
+        // other wise, look back at down and right position.
+        int[][] dp = new int[m][n];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                // bottom right the most(starting point)
+                if (i == m - 1 && j == n - 1) {
+                    dp[i][j] = grid[i][j];
+                }
+                // bottom level, look back in right
+                else if (i == m - 1 && j != n - 1) {
+                    dp[i][j] = grid[i][j] + dp[i][j + 1];
+                } 
+                // rightMost col, look back in down
+                else if (j == n - 1 && i != m - 1) {
+                    dp[i][j] = grid[i][j] + dp[i + 1][j];
+                } 
+                // otherwise
+                else {
+                    dp[i][j] = grid[i][j] + Math.min(dp[i + 1][j], dp[i][j + 1]);
+                } 
+            }
+        }
+        return dp[0][0];
+
+        // Method 2: use current matrix not taking extra space.
+        /*
+        public int minPathSum(int[][] grid) {
+            for (int i = grid.length - 1; i >= 0; i--) {
+                for (int j = grid[0].length - 1; j >= 0; j--) {
+                    if(i == grid.length - 1 && j != grid[0].length - 1)
+                        grid[i][j] = grid[i][j] +  grid[i][j + 1];
+                    else if(j == grid[0].length - 1 && i != grid.length - 1)
+                        grid[i][j] = grid[i][j] + grid[i + 1][j];
+                    else if(j != grid[0].length - 1 && i != grid.length - 1)
+                        grid[i][j] = grid[i][j] + Math.min(grid[i + 1][j],grid[i][j + 1]);
+                }
+            }
+            return grid[0][0];
+        }
+        */
+    }
+}
