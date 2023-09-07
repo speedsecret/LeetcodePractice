5e030dc1ae24ca9a67a8cda7d3ba035183146ac1
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-09-07 10:57:54 -0700

    Update and rename Construct Tree with inorder arrays and preorder array.java to 105. Construct Tree with inorder arrays and preorder array.java
    preorder array with an inorder array.

--- src/HashMap/105. Construct Tree with inorder arrays and preorder array.java
+++ src/HashMap/105. Construct Tree with inorder arrays and preorder array.java
@@ -1,4 +1,6 @@
 /*
+105. Construct Tree with inorder arrays and preorder array
+https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
 
 Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree 
 and inorder is the inorder traversal of the same tree, construct and return the binary tree.
@@ -10,6 +12,16 @@ Output: [3,9,20,null,null,15,7]
 Example 2:
 Input: preorder = [-1], inorder = [-1]
 Output: [-1]
+
+Constraints:
+
+1 <= preorder.length <= 3000
+inorder.length == preorder.length
+-3000 <= preorder[i], inorder[i] <= 3000
+preorder and inorder consist of unique values.
+Each value of inorder also appears in preorder.
+preorder is guaranteed to be the preorder traversal of the tree.
+inorder is guaranteed to be the inorder traversal of the tree.
 */
 
 /**
