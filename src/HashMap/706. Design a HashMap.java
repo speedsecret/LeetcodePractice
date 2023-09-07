55aa29fdd200d634185bcdcd1abb69080c0a36ad
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-09-07 13:15:29 -0700

    Update 706. Design a HashMap.java
    Implement the hashMap by using a ListNode[], and implement few API, such as get(), put(), remove();
    More importantly, we need to define the size, multiple values.

--- src/HashMap/706. Design a HashMap.java
+++ src/HashMap/706. Design a HashMap.java
@@ -32,5 +32,79 @@ myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
 myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]
 */
 
+class MyHashMap {
+    // Got to resolve the conflicts by using an ListNode array
+    // Then to calculate the hash by leveraging a hash function
+    class ListNode {
+        int key;
+        int value;
+        ListNode next;
+        public ListNode(int key, int value, ListNode next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    // Just the prime number which is reasonable
+    static final int size = 19997;
+    static final int mult = 12582917;
+    ListNode[] data;
+
+    public MyHashMap() {
+        this.data = new ListNode[size];
+    }
+    
+    public void put(int key, int value) {
+        remove(key);
+        int h = hash(key);
+        ListNode node = new ListNode(key, value, data[h]);
+        data[h] = node;
+    }
+    
+    public int get(int key) {
+        int h = hash(key);
+        ListNode node = data[h];
+        for (; node != null; node = node.next) {
+            if (node.key == key) {
+                return node.value;
+            }
+        }
+        return -1;
+    }
+    
+    public void remove(int key) {
+        int h = hash(key);
+        ListNode node = data[h];
+        if (node == null) {
+            return;
+        }
+        if (node.key == key) {
+            data[h] = node.next;
+        } else {
+            // if not able to locate the current element
+            // check the next element.
+            for (; node.next != null; node = node.next) {
+                if (node.next.key == key) {
+                    node.next = node.next.next;
+                    return;
+                }
+            }
+        }
+    }
+
+    private int hash(int key) {
+        return (int)((long)key * mult % size);
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
+
 
 
