23c83aa3be2b52695bd0e5979664fc47e2489eb7
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-09-14 11:25:01 -0700

    Update Longest Palindrome Substring.java

--- src/Two Pointers/Longest Palindrome Substring.java
+++ src/Two Pointers/Longest Palindrome Substring.java
@@ -15,9 +15,23 @@ Example 2:
 Input: s = "cbbd"
 Output: "bb"
 */
-Method:
-1.Two pointers
-2.Expand from Center
+// Method:
+// 1.Two pointers
+// 2.Expand from Center
+
+// when maxLen is odd. In a 0-indexed array, we need to adjust the left index such that it points to the start of the palindrome. Here's why:
+// When maxLen is odd, (maxLen - 1) / 2 calculates half of the length minus one. This is because, in a palindrome like "racecar," you need to move maxLen / 2 steps
+// from the center to reach the start or end.
+// By subtracting 1, you adjust for the 0-based index. In a 0-indexed array, the center i is located at the exact middle element of the palindrome.
+// So, to move to the start of the palindrome, you subtract 1.
+// When maxLen is even, take 
+//     i i+1
+// 0 1 2 3   4 
+// c c e e   c
+// maxLength = 4
+// l = i - (maxLength - 1) / 2 --> l = 2 - (4 - 1)/2 = 1;
+// r = i + maxLength / 2 --> 2 + 2 = 4;
+// if without subtracting one, we would encounter left = 0, right = 4 which is incorrect.
 
 class Solution {
     public String longestPalindrome(String s) {
