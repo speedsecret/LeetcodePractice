5787c6de8bf7420f922f69a554a305606b6c261b
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-09-16 09:58:19 -0700

    Create 1306. Jump Game III.java

---
+++ src/DFS/1306. Jump Game III.java
@@ -0,0 +1,48 @@
+/*
+Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach any index with value 0.
+
+Notice that you can not jump outside of the array at any time.
+
+ 
+
+Example 1:
+
+Input: arr = [4,2,3,0,3,1,2], start = 5
+Output: true
+Explanation: 
+All possible ways to reach at index 3 with value 0 are: 
+index 5 -> index 4 -> index 1 -> index 3 
+index 5 -> index 6 -> index 4 -> index 1 -> index 3 
+Example 2:
+
+Input: arr = [4,2,3,0,3,1,2], start = 0
+Output: true 
+Explanation: 
+One possible way to reach at index 3 with value 0 is: 
+index 0 -> index 4 -> index 1 -> index 3
+Example 3:
+
+Input: arr = [3,0,2,1,2], start = 2
+Output: false
+Explanation: There is no way to reach at index 1 with value 0.
+*/
+
+// Methodology:
+// The target position would be either from start to i + arr[i] or i - arr[i]
+// Hence, instead of using BFS, we should possibly adopt a quick solution, DFS.
+// The terminal condition is the current start not within the correct boundary or we have already visited the spots before.
+// Need to make sure the *arr[start] >= 0*
+
+
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        if (start >= 0 && start < arr.length && arr[start] >= 0) {
+            if (arr[start] == 0) {
+                return true;
+            }
+            arr[start] = -arr[start];
+            return canReach(arr, start + arr[start]) || canReach(arr, start - arr[start]);
+        }
+        return false;
+    }
+}
