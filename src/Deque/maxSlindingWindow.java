c404245ec647108465cf5ed172a0994e0740a211
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-09-16 21:50:39 -0700

    Update maxSlindingWindow.java

--- src/Deque/maxSlindingWindow.java
+++ src/Deque/maxSlindingWindow.java
@@ -24,6 +24,9 @@ Output: [1]
 */
 
 // Use monotonic queue.
+// two stages:
+// 1st stage: elements' index less than k, we should just adding or poll elements' index into it
+// 2st stage: check the peekFirst() element's index, if it equals to i - k, remove it, repeat stage 1 while loop, record the current max.
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         Deque<Integer> dq = new ArrayDeque<>();
@@ -32,6 +35,7 @@ class Solution {
 
         // keep the dp as a strictly decreasing order queue
         // monotonic Deque
+        // in another word, the peekFirst() ele must be the largest in the dq --> is currently the returnable max.
         for (int i = 0; i < k; i++) {
             while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
                 dq.pollLast();
@@ -43,6 +47,7 @@ class Solution {
         // track the element from index = k to index = n - 1;
         // sliding window needs running, we need to remove element out of the queue
         for (int i = k; i < nums.length; i++) {
+            // if it is current out of the scope, we should poll it out
             if (dq.peekFirst() == i - k) {
                 dq.pollFirst();
             }
@@ -56,4 +61,41 @@ class Solution {
 
         return arr;
     }
+
+/*
+ public int[] maxSlidingWindow(int[] nums, int k) {
+        // Method 1: use PriorityQueue to store the max element(maxHeap) and two pointer --> TTL due to remove time is really time consuming
+        // Method 2: using a treeSet to store k elements all the time but the time complexity is also huge --> O(nlogk)
+        // Method 3: using a deque, to only keep the useful elements in the deque and always keep a monotonic order queue
+        // Additionally, only store the index in the queue
+        Deque<Integer> dq = new ArrayDeque<>();
+        int[] arr = new int[nums.length - k + 1];
+        int start = 0;
+        // loop the nums once
+        // case1: for index < k
+        for (int i = 0; i < k; i++) {
+            int ele = nums[i];
+            // always maintain a increasing queue.
+            while (!dq.isEmpty() && ele >= nums[dq.peekLast()]) {
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+        } 
+        arr[start++] = nums[dq.peekFirst()];
+        // case2: for index >= k
+        //      thread2.1 for the first element in deque --> pollElement out
+        //      thread2.2 for check if the current element is larger or equals to smallest one in the queue(we should keep a fresh sliding window) --> keep adding element and poll element out if find larger or equal element
+        for (int i = k; i < nums.length; i++) {
+            if (dq.peekFirst() == i - k) {
+                dq.pollFirst();
+            }
+            while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+            arr[start++] = nums[dq.peekFirst()];
+        }
+        return arr;
+    }
+ */
 }
