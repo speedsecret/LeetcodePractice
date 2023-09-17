579ebd27a46ac37dcf303329d9d810fb2e04f5af
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-09-16 22:23:34 -0700

    Create 238. Product of Array Except Self.java

---
+++ src/Array/238. Product of Array Except Self.java
@@ -0,0 +1,50 @@
+/*
+238. Product of Array Except Self.java
+https://leetcode.com/problems/product-of-array-except-self/
+
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+
+Example 2:
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+
+Constraints:
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+*/
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        // Method1: use two arrays
+        // int[] leftArr and int[] rightArr
+        
+        // then we will update the final array once (by multiply left and right)
+
+        // Method2: use just the return answer array
+        // Loop the nums once, set up the answer array
+        // answer[i] represent that all product from index from 0 to i - 1;
+        // use rightProd represent that all product from currentIndex + 1 to the right handside
+        int[] answer = new int[nums.length];
+        answer[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            answer[i] = nums[i - 1] * answer[i - 1];
+        }
+        int rightProd = 1; 
+        // from right to the left
+        for (int i = nums.length - 1; i >= 0; i--) {
+            answer[i] *= rightProd;
+            rightProd *= nums[i];
+        }
+        return answer;
+    }
+}
