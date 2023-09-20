3466b71bbe8d5fe16b0f3e0bf1be0295b86e4a9f
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-09-19 18:41:49 -0700

    Update and rename WordLadder.java to 127. WordLadder.java

--- src/BFS/127. WordLadder.java
+++ src/BFS/127. WordLadder.java
@@ -1,3 +1,25 @@
+/*
+127. WordLadder.java
+https://leetcode.com/problems/word-ladder/description/
+
+A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
+
+Every adjacent pair of words differs by a single letter.
+Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+sk == endWord
+Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+Example 1:
+
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+Output: 5
+Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+Example 2:
+
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+Output: 0
+Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
+*/
+
 package BFS;
 
 import java.util.ArrayDeque;
@@ -14,7 +36,7 @@ class WordLadder {
         // create a new hashSet, we would like to check if the new elements already in the hashSet
         // once we poll the next level elements from queue, the steps + 1
         // we would start from beginWord, replace every character from 'a' to 'z'
-        // once we met the endWord, return the steps right away.
+        // once met the endWord, return the steps right away.
         // if we exhausted all options and the current queue is empty, return 0
         Set<String> set = new HashSet<>(wordList);
         if (!set.contains(endWord)) {
@@ -41,7 +63,7 @@ class WordLadder {
                     for (char ch = 'a'; ch <= 'z'; ch++) {
                         curWordArr[j] = ch;
                         String newString = new String(curWordArr);
-                        if (!visited.contains(newString) && set.contains(newString)) {
+                        if (visited.add(newString) && set.contains(newString)) {
                             queue.addFirst(newString);
                             visited.add(newString);
                         }
