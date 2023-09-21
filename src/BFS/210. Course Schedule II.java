d5daf89ff5ebe4ab9d77fff97df654ee0182b520
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-09-21 10:21:47 -0700

    Update and rename Course Schedule II.java to 210. Course Schedule II.java

--- src/BFS/210. Course Schedule II.java
+++ src/BFS/210. Course Schedule II.java
@@ -1,4 +1,7 @@
 /*
+210. Course Schedule II.java
+https://leetcode.com/problems/course-schedule-ii/
+
 There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. 
 You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must
 take course bi first if you want to take course ai.
@@ -24,6 +27,14 @@ Input: numCourses = 1, prerequisites = []
 Output: [0]
 */
 
+// Methodology
+// adjList to construct the relationship between courses
+// hashMap record the course status: 
+// 0 -> not visited
+// 1 -> processing
+// 2 -> visited
+
+// for DFS method, there are two terminal conditions.
 
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
