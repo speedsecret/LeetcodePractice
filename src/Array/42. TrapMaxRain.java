99caafd7f1c8bd562bdc3b0ffa966818d553bc37
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-09-24 13:03:53 -0700

    Update and rename TrapMaxRain.java to 42. TrapMaxRain.java

--- src/Array/42. TrapMaxRain.java
+++ src/Array/42. TrapMaxRain.java
@@ -1,4 +1,27 @@
-package Array;
+/*
+42. TrapMaxRain.java
+https://leetcode.com/problems/trapping-rain-water/description/
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+Example 1:
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+Example 2:
+Input: height = [4,2,0,3,2,5]
+Output: 9
+ 
+
+Constraints:
+n == height.length
+1 <= n <= 2 * 104
+0 <= height[i] <= 105
+*/    
+
+// Methodology:
+// Use two pointers: curLeft, curRight to check from left, right to middle
+// Use two int variables leftMax, rightMax, and updated the leftMax, rightMax on the fly
+// when curLeft < curRight, check the curLeft with LeftMax, vice verse when curLeft > curRight
 
 public class TrapMaxRain {
     public static int trap(int[] height) {
