25faea38cfed3335e8cf7c8139c4e14fd4c1cad6
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-09-26 16:31:01 -0700

    Update and rename AllSubsets.java to 78.AllSubsets.java

--- src/DFS/78.AllSubsets.java
+++ src/DFS/78.AllSubsets.java
@@ -1,8 +1,27 @@
-package DFS;
+/*
+78.AllSubsets.java
+https://leetcode.com/problems/subsets/description/
 
-import java.util.ArrayList;
-import java.util.List;
 
+Given an integer array nums of unique elements, return all possible 
+subsets(the power set). The solution set must not contain duplicate subsets. Return the solution in any order.
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+All the numbers of nums are unique.
+*/
 
 public class AllSubsets {
     public static List<String> allSubsets(String str) {
