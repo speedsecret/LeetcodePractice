a76de8e2ff7b708c2f385d87fdca545e552abbb7
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-09-30 20:52:58 -0700

    Update 706. Design a HashMap.java

--- src/HashMap/706. Design a HashMap.java
+++ src/HashMap/706. Design a HashMap.java
@@ -32,44 +32,52 @@ myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
 myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]
 */
 
-class MyHashMap {
-    // Got to resolve the conflicts by using an ListNode array
-    // Then to calculate the hash by leveraging a hash function
-    class ListNode {
-        int key;
-        int value;
-        ListNode next;
-        public ListNode(int key, int value, ListNode next) {
-            this.key = key;
-            this.value = value;
-            this.next = next;
-        }
-    }
+// Methodology:
+// We will use a ListNode class
+// There are int key, value and ListNode available in this class
+// create a listNode[] array, with size(~10^4)
+// critical function is to define the hash(key) ---> (int)((long)key * multi % size);
 
-    // Just the prime number which is reasonable
-    static final int size = 19997;
-    static final int mult = 12582917;
+class ListNode{
+    int key;
+    int value;
+    ListNode next;
+    // the constructor define the relationship between current node with next node
+    public ListNode(int key, int value, ListNode next) {
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+}
+
+class MyHashMap {
+    static int size = 19997;
+    static int multi = 12582917;
     ListNode[] data;
 
     public MyHashMap() {
-        this.data = new ListNode[size];
+        // determine the size of the hashMap, which means
+        // the ListNode array can contain size length of listNodes.
+        data = new ListNode[size];
     }
     
     public void put(int key, int value) {
         remove(key);
         int h = hash(key);
-        ListNode node = new ListNode(key, value, data[h]);
-        data[h] = node;
+        ListNode newNode = new ListNode(key, value, data[h]);
+        data[h] = newNode;
     }
     
     public int get(int key) {
         int h = hash(key);
         ListNode node = data[h];
-        for (; node != null; node = node.next) {
+        while (node != null) {
             if (node.key == key) {
                 return node.value;
             }
+            node = node.next;
         }
+        // can not find the node
         return -1;
     }
     
@@ -80,23 +88,33 @@ class MyHashMap {
             return;
         }
         if (node.key == key) {
+            // instead of keep the node in data[h]
+            // we would need to update the date[h] by
+            // using node.next;
             data[h] = node.next;
         } else {
-            // if not able to locate the current element
-            // check the next element.
-            for (; node.next != null; node = node.next) {
+            while (node.next != null) {
                 if (node.next.key == key) {
                     node.next = node.next.next;
                     return;
                 }
+                node = node.next;
             }
         }
     }
 
     private int hash(int key) {
-        return (int)((long)key * mult % size);
+        return (int)((long) key * multi % size);
     }
 }
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
 
 /**
  * Your MyHashMap object will be instantiated and called as such:
