492423e0a72db15d39a411cdc3b72cdf51e7af2f
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-10-01 20:36:06 -0700

    Update 146. LRU.java

--- src/LinkedList/146. LRU.java
+++ src/LinkedList/146. LRU.java
@@ -97,3 +97,98 @@ class LRUCache {
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
+
+/* 
+[Nothing special]10.01.2023 Version
+
+class Node {
+    int key;
+    int value;
+    Node prev;
+    Node next;
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+class LRUCache {
+    Map<Integer, Node> cache;
+    int size;
+    Node head, tail;
+    int capacity;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        cache = new HashMap<>();
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key) {
+        Node node = cache.get(key);
+        if (node != null) {
+            int res = node.value;
+            updateNode(node);
+            return res;
+        } else {
+            return -1;
+        }
+    }
+    
+    public void put(int key, int value) {
+        Node node = cache.get(key);
+        if (node == null) {
+            Node newNode = new Node(key, value);
+            size++;
+            // check size
+            if (size > capacity) {
+                Node popNode = pop();
+                cache.remove(popNode.key);
+                size--;
+            }
+            addNode(newNode);
+            cache.put(key, newNode);
+        } else {
+            // update the node value to its new value
+            node.value = value;
+            updateNode(node);
+            cache.put(key, node);
+        }
+    }
+
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void updateNode(Node node) {
+        remove(node);
+        addNode(node);
+    }
+
+    private void addNode(Node node) {
+        node.next = head.next;
+        head.next.prev = node;
+
+        node.prev = head;
+        head.next = node;
+    }
+
+    private Node pop() {
+        Node popNode = tail.prev;
+        remove(popNode);
+        return popNode;
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+*/
