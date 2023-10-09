507e3708fbdd3c79d087d9697597a740eb5a56cd
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-10-09 15:56:51 -0700

    Update 22. Generate Parenthese.java

--- src/DFS/22. Generate Parenthese.java
+++ src/DFS/22. Generate Parenthese.java
@@ -24,6 +24,42 @@ Constraints:
 // Methodology
 // Use DFS with int variable left, right to record how many left/right bracket still remained to use.
 
+// 10.09.2023 version
+// note: we still need backtrack.
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        // use two int variables, left and right
+        // and initilize left equals to n, right equals to n too.
+        List<String> res = new ArrayList<>();
+        dfs(res, new StringBuilder(), n, n);
+        return res;
+    }
+
+    private void dfs(List<String> res, StringBuilder sb, int leftRemain, int rightRemain) {
+        // base case
+        if (leftRemain == 0 && rightRemain == 0) {
+            res.add(new String(sb));
+            return;
+        }
+        if (leftRemain < 0 || rightRemain < 0 || leftRemain > rightRemain) {
+            return;
+        }
+        // recursive rules:
+        // No.1 check the leftRemain
+        if (leftRemain > 0) {
+            sb.append('(');
+            dfs(res, sb, leftRemain - 1, rightRemain);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        // No.2 check the rightRemain, it must have more leftover than leftRemain
+        if (rightRemain > leftRemain) {
+            sb.append(')');
+            dfs(res, sb, leftRemain, rightRemain - 1);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
+
 class Solution {
     public List<String> generateParenthesis(int n) {
         // DFS use stringBuilder
