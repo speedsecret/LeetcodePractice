d751e10eb495a1395a91e08d42cdc571a03bebd6
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-10-16 19:02:38 -0700

    Create 97. Interleaving String.java

---
+++ src/DP/97. Interleaving String.java
@@ -0,0 +1,62 @@
+/*
+97. Interleaving String.java
+Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2.
+https://leetcode.com/problems/interleaving-string/description/
+
+An interleaving of two strings s and t is a configuration where s and t are divided into n and m 
+substrings. 
+
+Example 1:
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+Output: true
+Explanation: One way to obtain s3 is:
+Split s1 into s1 = "aa" + "bc" + "c", and s2 into s2 = "dbbc" + "a".
+Interleaving the two splits, we get "aa" + "dbbc" + "bc" + "a" + "c" = "aadbbcbcac".
+Since s3 can be obtained by interleaving s1 and s2, we return true.
+Example 2:
+
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+Output: false
+Explanation: Notice how it is impossible to interleave s2 with any other string to obtain s3.
+Example 3:
+
+Input: s1 = "", s2 = "", s3 = ""
+Output: true
+ 
+
+Constraints:
+
+0 <= s1.length, s2.length <= 100
+0 <= s3.length <= 200
+s1, s2, and s3 consist of lowercase English letters.
+ 
+
+Follow up: Could you solve it using only O(s2.length) additional memory space? --> use one int[] dp array instead could solve this bonus question.
+*/
+
+public class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s3.length() != s1.length() + s2.length()) {
+            return false;
+        }
+        boolean dp[][] = new boolean[s1.length() + 1][s2.length() + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= s1.length(); i++) {
+            // considering using s2 in the current stage
+            // hence it is essential to make sure that s1.charAt(i - 1) == s3.charAt(i - 1);
+            dp[i][0] = dp[i - 1][0] && s1.charAt(i - 1) == s3.charAt(i - 1);
+        }
+        for (int j = 1; j <= s2.length(); j++) {
+            // considering using s1 in the current stage
+            // hence it is essential to make sure that s2.charAt(i - 1) == s3.charAt(i - 1);
+            dp[0][j] = dp[0][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+        }
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                // either choose s2 in the current stage or choose s1 in the current stage
+                dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
+}
