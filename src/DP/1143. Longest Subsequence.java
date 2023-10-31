5bcdb9fee72d30d731b396d8eb645121cab64ec4
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-10-30 23:46:55 -0700

    Create 1143. Longest Subsequence.java

---
+++ src/DP/1143. Longest Subsequence.java
@@ -0,0 +1,59 @@
+/*
+Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings.
+Example 1:
+
+Input: text1 = "abcde", text2 = "ace" 
+Output: 3  
+Explanation: The longest common subsequence is "ace" and its length is 3.
+Example 2:
+
+Input: text1 = "abc", text2 = "abc"
+Output: 3
+Explanation: The longest common subsequence is "abc" and its length is 3.
+Example 3:
+
+Input: text1 = "abc", text2 = "def"
+Output: 0
+Explanation: There is no such common subsequence, so the result is 0.
+*/
+
+// Methodology:
+// Use 2D array to check each possible char accordingly.
+
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {    
+        // If text1 doesn't reference the shortest string, swap them.
+        if (text2.length() < text1.length()) {
+            String temp = text1;
+            text1 = text2;
+            text2 = temp;
+        }
+        
+        // The previous and current column starts with all 0's and like 
+        // before is 1 more than the length of the first word.
+        int[] previous = new int[text1.length() + 1];
+        int[] current = new int[text1.length() + 1];
+        
+        // Iterate through each column, starting from the last one.
+        for (int col = text2.length() - 1; col >= 0; col--) {
+            for (int row = text1.length() - 1; row >= 0; row--) {
+                if (text1.charAt(row) == text2.charAt(col)) {
+                    current[row] = 1 + previous[row + 1];
+                } else {
+                    current[row] = Math.max(previous[row], current[row + 1]);
+                }
+            }
+            // The current column becomes the previous one, and vice versa.
+            int[] temp = previous;
+            previous = current;
+            current = temp;
+        }
+        
+    // The original problem's answer is in previous[0]. Return it.
+    return previous[0];
+  }
+}
