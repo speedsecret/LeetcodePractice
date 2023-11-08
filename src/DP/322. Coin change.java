b2ff93dfe37dcfe1a4c568578d23b81fc949d1b1
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-11-07 18:33:01 -0800

    Create 322. Coin change.java

---
+++ src/DP/322. Coin change.java
@@ -0,0 +1,47 @@
+/*
+322. Coin change.java
+https://leetcode.com/problems/coin-change/description/
+
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+You may assume that you have an infinite number of each kind of coin.
+Example 1:
+
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+Example 2:
+
+Input: coins = [2], amount = 3
+Output: -1
+Example 3:
+
+Input: coins = [1], amount = 0
+Output: 0
+
+*/
+
+// Methodology
+// To solve a large problem, we may want to find the smaller problem and solve it earlier.
+
+class Solution {
+  public int coinChange(int[] coins, int amount) {
+      int[] dp = new int[amount + 1];
+      Arrays.fill(dp, amount + 1);
+      // base case:
+      dp[0] = 0;
+
+      // induction rule, what is the smaller size of the problem
+      // then we can solve this problem on the top of the smaller problem.
+      for (int i = 1; i <= amount; i++) {
+          for (int coin : coins) {
+              if (coin <= i) {
+                  dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+              }
+          }
+      }
+      return dp[amount] > amount ? -1 : dp[amount];
+  }
+}
