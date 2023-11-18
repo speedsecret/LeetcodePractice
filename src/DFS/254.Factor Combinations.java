71e8d4035efcde1e5e22253e83996262be07e6e1
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-11-18 11:19:20 -0800

    Update 254.Factor Combinations.java

--- src/DFS/254.Factor Combinations.java
+++ src/DFS/254.Factor Combinations.java
@@ -22,6 +22,25 @@ Example 3:
 Input: n = 37
 Output: []
 */
+// Methodology
+// Use DFS, which is extremly intricating.
+// Started wiz a linkedList which size == 1, and it only containing the number of n.
+// we would check all possible combination factors from the largest n to 2.
+// base case: the size of inner list is larger than 1--> list > 1 --> adding it into the final output
+// generate an int value: lastFactor;
+// recursive rule:  i started from 2 if the list is empty, if not, we would started from the lastElement from the list
+// all the way to lastFactor / i inclusively;
+
+/*
+The main logic for backtracking helper function is as follows:
+
+1. If factors.size() > 1, add a copy of it into ans since it's one of the desired solutions.
+2. Get the last element of factors lastFactor and remove it from factors.
+3. If factors is empty, iterate over i from 2, otherwise loop i from the last value in factors. Iterate until i > lastFactor / i.
+4. For each i, if lastFactor % i == 0, put i and lastFactor / i in the factors list and call backtracking(factors, ans).
+5. Restore the list (backtrack) factors by removing the last 2 elements in factors.
+6. Restore the list (backtrack) factors by adding the lastFactor back.
+*/
 
 class Solution {
     public List<List<Integer>> getFactors(int n) {
@@ -56,4 +75,3 @@ class Solution {
         list.add(lastFactor);
     }
 }
-
