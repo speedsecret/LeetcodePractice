87ee9b57abef4caf5e10b88ea3fe0ffb222e2c19
speedsecret <82914438+speedsecret@users.noreply.github.com>
2023-12-29 16:36:16 -0800

    Update 133. Clone Graph.java

--- src/Graph/133. Clone Graph.java
+++ src/Graph/133. Clone Graph.java
@@ -12,15 +12,13 @@ class Node {
     public int val;
     public List<Node> neighbors;
 }
+
 Test case format:
-
-For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
-
+For simplicity, each node's value is the same as the node's index (1-indexed). 
+For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
 An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
-
 The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
 */
-
 /*
 // Definition for a Node.
 class Node {
@@ -57,7 +55,7 @@ class Solution {
         Deque<Node> queue = new ArrayDeque<>();
         Map<Node, Node> visited = new HashMap<>();
         queue.offerLast(node);
-        // Firstly, we should initilizae the new node
+        // Firstly, we should initilize the new node
         visited.put(node, new Node(node.val, new ArrayList<>()));
         // check if the queue is empty
         while (!queue.isEmpty()) {
